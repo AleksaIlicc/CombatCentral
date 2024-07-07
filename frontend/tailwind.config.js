051f27453a5e23847a3fae40b2e01c1a5433d8d1
AleksaIlicc <aleksa.ilic.dv@outlook.com>
2024-07-07 18:11:43 +0200

    configured global styling variables

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: '#ab1c29',
+        'transparent-red': '#ab1c29',
+        black: '#270808',
+        gray: '#555c73',
+      },
+    },
   },
   plugins: [],
 };
