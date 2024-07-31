a16cecde4b3ad4f08eacf045c109e7199212b3db
AleksaIlicc <aleksa.ilic.dv@outlook.com>
2024-07-31 04:20:15 +0200

    made module for auth, small changes in ui on frontend

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService
+) {}
 
   @Post('/register')
-  async register(@Body() body) {
+  async register(@Body() body: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(body.password, 10);
-    return this.authService.create({
+    this.authService.create({
       ...body,
       password: hashedPassword,
     });
---
+++ backend/src/api/auth/dtos/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  email: string;
+  fullName: string;
+  username: string;
+  password: string;
+}
